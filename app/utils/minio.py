b574d159fe9fcaadc735276ae0a3b46be5fa1db2
Krisa29 <sathvika.ganni@gmail.com>
2025-05-02 14:56:27 -0400

    Resolving Issue 6

--- app/utils/minio.py
+++ app/utils/minio.py
@@ -6,17 +6,21 @@ minio_client = Minio(
     settings.minio_endpoint,
     access_key=settings.minio_access_key,
     secret_key=settings.minio_secret_key,
-    secure=settings.minio_base_url
+    secure=settings.minio_use_ssl
 )
 
-def upload_image_to_minio(file_obj, content_type):
-    filename = f"{uuid.uuid4()}.jpg"
+def upload_image_to_minio(file_obj, content_type, filename):
+    ext = filename.split('.')[-1]
+    unique_filename = f"{uuid.uuid4()}.{ext}"
     minio_client.put_object(
         settings.minio_bucket_name,
-        filename,
+        unique_filename,
         file_obj,
         length=-1,
         part_size=10 * 1024 * 1024,
         content_type=content_type
     )
-    return f"{settings.minio_base_url}/{settings.minio_bucket_name}/{filename}" 
+    return f"{settings.minio_base_url}/{settings.minio_bucket_name}/{unique_filename}"
+
+def get_image_url_from_minio(file_name: str) -> str:
+    return f"{settings.minio_base_url}/{settings.minio_bucket_name}/{file_name}"
