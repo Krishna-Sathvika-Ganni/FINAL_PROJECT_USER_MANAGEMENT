042e1a7bf4e86991294a40c51e13cd5f41a2b296
Krisa29 <sathvika.ganni@gmail.com>
2025-05-05 12:15:21 -0400

    Added test case to check for URL generation

---
+++ tests/test_utils/test_minio.py
@@ -0,0 +1,22 @@
+import io
+from unittest.mock import patch
+from app.utils.minio import upload_image_to_minio,get_image_url_from_minio
+
+@patch("app.utils.minio.minio_client.put_object")
+def test_upload_image_to_minio(mock_put_object):
+    dummy_file = io.BytesIO(b"dummy image data")
+    dummy_file.seek(0)
+    content_type = "image/jpeg"
+    filename = "test.jpg"
+
+    url = upload_image_to_minio(dummy_file, content_type, filename)
+
+    assert url.endswith(".jpg")
+    mock_put_object.assert_called_once()
+
+def test_get_image_url_from_minio():
+    file_name = "example.jpg"
+    url = get_image_url_from_minio(file_name)
+
+    assert file_name in url
+    assert url.startswith("http") or url.startswith("https")
